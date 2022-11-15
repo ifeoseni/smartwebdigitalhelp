117aec12c40a50a32778d67aec967662dc462f58
Ifeoluwa Oseni <ifeoseni@gmail.com>
2022-11-16 00:22:49 +0100

    StartingDigitalHelpFromSmartWebDigitalHelpCode

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,24 @@
+<footer class="container-fluid bg-info">
+    <div class="row">
+        <h2 class="text-center">Our Location</h2>
+        <p class="text-center">Digital Help is always accessible and available to everyone. We are just a click away. We have a strong presence in the following states</p>
+    </div>
+    <div class="row">
+        <div class="col-sm-12 col-md-3 col-lg-3">
+            <h3 class="text-center fw-bold">Akwa Ibom State</h3>
+            <p>Our team lead and marketing head served in Akwa Ibom state. Areas in Akwa Ibom where we have strong presence include Oron Road, Nwaniba, Ikpa Road etc.</p>
+        </div>
+        <div class="col-sm-12 col-md-3 col-lg-3">
+            <h3 class="text-center fw-bold">FCT Nigeria</h3>
+            <p>Abuja is the capital of Nigeria and we have some of our most capable hands living in the capital of the most populous black Nation on planet Earth.</p>
+        </div>
+        <div class="col-sm-12 col-md-3 col-lg-3">
+            <h3 class="text-center fw-bold">Lagos State</h3>
+            <p>Lagos is always rated as the number 1 state in Nigeria despite being one of the smallest states by land mass, most of our team members  stay in Lagos.</p>
+        </div>
+        <div class="col-sm-12 col-md-3 col-lg-3">
+            <h3 class="text-center fw-bold">Oyo State</h3>
+            <p>We once had a strong community at the university of Ibadan during our schooling days. Most of our members once paced through this great citadel of learning</p>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,46 @@
+<!-- resources/views/child.blade.php -->
+ 
+@extends('layouts.header')
+ 
+@section('title', 'Home Page')
+@section('description', 'Digital Help')
+ 
+ 
+@section('content')
+    <div class="container-fluid bg-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-lg-4">
+                    <h3 class="text-dark">65</h3>
+                    <h4>Hours Spent On Projects</h4>
+                </div>
+                <div class="col-md-4 col-lg-4">
+                    <h3 class="text-dark">8</h3>
+                    <h4>Projects Worked On</h4>
+                </div>
+                <div class="col-md-4 col-lg-4">
+                    <h3 class="text-dark">5</h3>
+                    <h4>Organization Worked With</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <h3 class="text-center">Our Mission</h3>
+            <h4>Our mission is to be of help to a million people in Nigeria before the year 2040. Digital Help aim is to help Nigerians use technology to better their lives, work and processes.</h4>
+        </div>
+        <div class="row">
+            <h3 class="text-center">Our Services</h3>
+            <h4>We are skilled in several things but our super power comes from using a computer connected to the internet. We build websites and manage web projects. We also help people handle their digital marketing campaigns at an affordable and pocket friendly price. Need someone to give you an honest opinion about your digital effort or project, get in touch with Digital Help.</h4>
+        </div>
+    </div>
+    
+virtual assistant in nigeria by digital help
+Virtual Assistance
+
+A virtual assistant is a person that help you achieve your goal without being physically close to you. Need something to be done that can easily be done, then get a good virtual assistant from Digital Help today.
+Before you hire us, it is best you hear what people are saying about us on Google.	
+Check Digital Help On Google
+
+@endsection
